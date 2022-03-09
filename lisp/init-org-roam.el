84d010ab293ca3e69dc4c5858c6cbbcd51094ad4
Yang Chuang <ifrenzyc@gmail.com>
2022-03-09 17:27:07 +0800

    add org-roam

--- lisp/init-org-roam.el
+++ lisp/init-org-roam.el
@@ -7,14 +7,95 @@
 
 ;;; Code:
 
+
+;; org-roam-capture 主要参考： /Users/yangc/src/emacs.d/ody55eus-doom-emacs.d/doom/Emacs.org
 (use-package org-roam
-  ;; :straight (:host github :repo "org-roam/org-roam")
+  :straight (:host github :repo "org-roam/org-roam" :files (:defaults "extensions/*"))
+  :delight " 𝕫"
   :init
   ;; disable v1 migrate to v2 warning
   (setq org-roam-v2-ack t)
   :custom
   (org-roam-directory "~/notes/09_Zettelkästen")
+  (org-roam-capture-templates
+   '(("d" "default" plain "%?"
+      :if-new (file+head "${slug}.txt"
+               "#+TITLE: ${title}\n#+ROAM_TAGS:\n#+ROAM_KEY:\n")
+      :unnarrowed t)
+     ("p" "personal" plain "%?" :target
+      (file+head "Personal/%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+ROAM_TAGS:\n#+ROAM_KEY:\n") :unnarrowed t)
+     ("w" "work" plain "%?" :target
+      (file+head "Work/%<%Y%m%d%H%M%S>-${slug}.org"  "#+title: ${title}\n#+ROAM_TAGS:\n#+ROAM_KEY:\n") :unnarrowed t)
+     ("c" "default" plain
+      "%?"
+      :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+               "#+title: ${title}\n#+created: %U\n#+last_modified: %U\n\n")
+      :unnarrowed t)
+     ("r" "ref" plain
+      "%?"
+      :target (file+head "References/${citekey}.org"
+               "#+title: ${title}\n#+created: %U\n#+last_modified: %U\n\n")
+      :unnarrowed t)
+     ("p" "ref + physical" plain
+      "%?"
+      :target (file+head "References/${citekey}.org"
+               "#+title: ${title}\n#+created: %U\n#+last_modified: %U\n\n* Notes :physical:")
+      :unnarrowed t)
+     ("n" "ref + noter" plain
+      "%?"
+      :target (file+head "References/${citekey}.org"
+               ,(s-join "\n" (list "#+title: ${title}\n#+created: %U\n#+last_modified: %U\n"
+                                   "* Notes :noter:"
+                                   ":PROPERTIES:"
+                                   ":NOTER_DOCUMENT: %(orb-get-attached-file \"${citekey}\")"
+                                   ":NOTER_PAGE:"
+                                   ":END:")))
+      :unnarrowed t)))
+  (org-roam-capture-ref-templates
+   '(("r" "ref" plain
+      "%?"
+      :target (file+head "web/${slug}.org"
+               "#+title: ${title}\n#+roam_key: ${ref}\n#+created: %u\n#+last_modified: %U\n\n%(zp/org-protocol-insert-selection-dwim \"%i\")")
+      :unnarrowed t)
+     ("R" "Reference" plain
+      "%?\n\n* Citations\n#+begin_quote\n${body}\n#+end_quote"
+      :if-new (file+head
+               "Literature/%<%Y%m%d%H%M%S>-${slug}.org"
+               "#+title: ${title}\n#+date: %U\n")
+      :unnarrowed t
+      )
+     ("l" "Literature References" plain
+      "%?\n\n* Abstract\n#+begin_quote\n${body}\n#+end_quote"
+      :if-new (file+head
+               "References/%<%Y%m%d%H%M%S>-${slug}.org"
+               "#+title: ${title}\n#+date: %U\n#+ROAM_REF: ${ref}")
+      :unnarrowed t
+      :empty-lines 1)
+     ("w" "Web site" entry
+      :target (file+head
+               "Literature/%<%Y%m%d%H%M%S>-${slug}.org"
+               "#+title: ${title}\n#+date: %U\n")
+      "* %a :website:\n\n%U %?\n\n#+begin_quote\n%:initial\n#+end_quote")
+     ("i" "incremental" plain
+      "* %?\n%(zp/org-protocol-insert-selection-dwim \"%i\")"
+      :target (file+head "web/${slug}.org"
+               "#+title: ${title}\n#+roam_key: ${ref}\n#+created: %u\n#+last_modified: %U\n\n")
+      :unnarrowed t
+      :empty-lines-before 1)))
+  (org-roam-dailies-capture-templates
+   '(("d" "daily" entry
+      "* %i%?"
+      :target (file+head "daily-%<%Y-%m-%d>.org"
+               "#+TITLE: %<%A, %d %b %Y>\n#+filetags: :daily-notes:\n\n"))
+     ))
   :config
+  (setq org-roam-graph-extra-config '(("overlap" . "prism")
+                                      ("color" . "skyblue")
+                                      ;; ("color" . "#DEDEFF")
+                                      ("shape" . "ellipse")
+                                      ("style" . "rounded,filled")
+                                      ("fillcolor" . "#EFEFFF")
+                                      ("fontcolor" . "#111111")))
   (setq org-roam-file-extensions '("txt"))
   (setq org-roam-graphviz-executable "/usr/local/Cellar/graphviz/2.44.1/bin/dot")
   (setq org-roam-graphviz-extra-options '(("overlap" . "false")))
@@ -22,6 +103,17 @@
   (setq org-roam-verbose t)
   (setq org-roam-db-location
         (concat org-roam-directory "/.database/org-roam.db"))
+
+  (setq org-refile-targets '((org-roam-list-files . (:maxlevel . 1))))
+
+  ;; display org-roam buffer to the right
+  (add-to-list 'display-buffer-alist
+               '("\\*org-roam\\*"
+                 (display-buffer-in-direction)
+                 (direction . right)
+                 (window-width . 0.33)
+                 (window-height . fit-window-to-buffer)))
+
   (org-roam-setup)
   :general
   (yc/leader-keys
@@ -48,14 +140,43 @@
   ;;   "rg" 'org-roam-show-graph)
   )
 
+(use-package company-org-roam
+  :after org-roam
+  :config
+  (add-to-list 'company-backends 'company-org-roam))
+
+(use-package org-roam-bibtex
+  :after org-roam
+  :config
+  (use-package org-ref)
+  (setq orb-file-field-extensions '("pdf" "epub"))
+  (org-roam-bibtex-mode 1))
+
 ;; https://github.com/org-roam/org-roam-server
 (use-package org-roam-server)
 
-(use-package org-roam-ui
-  :straight
-  (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
+(use-package org-roam-protocol
+  :straight nil
   :after org-roam
-  :hook (org-roam . org-roam-ui-mode))
+  :config
+  (require 'org-roam-protocol))
+
+(use-package websocket
+  :ensure t
+  :after org-roam)
+
+(use-package org-roam-ui
+  :straight (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
+  :after org-roam
+  :init
+  (when (featurep 'xwidget-internal)
+    (setq org-roam-ui-browser-function #'xwidget-webkit-browse-url))
+  :hook (org-roam . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
 
 ;; (use-package gkroam
 ;;   :init
