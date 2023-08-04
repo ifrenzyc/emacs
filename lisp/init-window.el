3e3ac191300cf68e0234a162009d022b25389a7a
Yang Chuang <ifrenzyc@gmail.com>
2023-08-04 12:20:12 +0800

    improve pixel scroll percision

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -40,7 +40,23 @@
 (use-package pixel-scroll-precision
   :ensure nil
   :init
-  (pixel-scroll-precision-mode))
+  (pixel-scroll-precision-mode t)
+  :config
+  (setq pixel-scroll-precision-interpolate-page t)
+  (defun +pixel-scroll-interpolate-down (&optional lines)
+    (interactive)
+    (if lines
+        (pixel-scroll-precision-interpolate (* -1 lines (pixel-line-height)))
+      (pixel-scroll-interpolate-down)))
+
+  (defun +pixel-scroll-interpolate-up (&optional lines)
+    (interactive)
+    (if lines
+        (pixel-scroll-precision-interpolate (* lines (pixel-line-height))))
+    (pixel-scroll-interpolate-up))
+
+  (defalias 'scroll-up-command '+pixel-scroll-interpolate-down)
+  (defalias 'scroll-down-command '+pixel-scroll-interpolate-up))
 
 ;; (use-package yascroll
 ;;   :init
