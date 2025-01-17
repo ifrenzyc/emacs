fc7014f0039410e529c9bf46ca7ddedeb722b35c
Yang Chuang <ifrenzyc@gmail.com>
2025-01-17 11:45:06 +0800

    统一 org-agenda-file 配置

--- lisp/init-org-agenda.el
+++ lisp/init-org-agenda.el
@@ -8,6 +8,7 @@
 ;; 
 
 ;;; Code:
+(require 'init-custom)
 (require 'init-org)
 
 ;; TODO: 具体配置参考这个：
@@ -176,27 +177,6 @@
   (setq org-agenda-skip-deadline-prewarning-if-scheduled '(pre-scheduled))
 
   (setq org-agenda-todo-ignore-scheduled 'future)
-
-  ;; TODO: 参考这篇 https://blog.jethro.dev/posts/processing_inbox/(file:~/src/emacs.d/jethrokuan-dots/.doom.d/config.el、~/src/emacs.d/Kinneyzhang-emacs.d/elisp/init-org.el::jethro/org-process-inbox)，设置 refile 的流程步骤
-  ;; 这里的思路：类似于其他的 GTD 软件，在整理 Inbox 的 todo 项时，要设置比如 tags、预计时间、项目等模板化信息，在 orgmode 中的思路则是通过调用一个组合的 function，依次调用相应的命令。
-
-  ;; http://cachestocaches.com/2016/9/my-workflow-org-agenda/#capture--refile
-  (setq org-directory "~/notes/Zettelkästen/")
-  (setq org-agenda-file-inbox (expand-file-name "0x00_GTD/00_inbox.txt" org-directory))
-  (setq org-agenda-file-notes (expand-file-name "0x00_GTD/00_notes.txt" org-directory))
-  (setq org-agenda-file-work (expand-file-name "0x00_GTD/01_Work.txt" org-directory))
-  (setq org-agenda-file-personal (expand-file-name "0x00_GTD/01_Personal.txt" org-directory))
-  (setq org-agenda-file-tickler (expand-file-name "0x00_GTD/02_tickler.txt" org-directory))
-  (setq org-agenda-file-beorg (expand-file-name "0x00_GTD/03_beorg-refile.txt" org-directory))
-  (setq org-agenda-file-agenda (expand-file-name "0x00_GTD/99_agenda.txt" org-directory))
-  (setq org-agenda-file-done (expand-file-name "0x00_GTD/done.org_archive" org-directory))
-  ;; (setq org-agenda-file-journal (expand-file-name "journal.txt" org-directory))
-  ;; (setq org-agenda-file-thoughts (expand-file-name "thoughts.txt" org-directory))
-  ;; (setq org-agenda-file-code-snippet (expand-file-name "snippet.txt" org-directory))
-  ;; (setq org-agenda-diary-file (expand-file-name "diary.txt" org-directory))
-  (setq org-default-notes-file (expand-file-name "0x00_GTD/00_inbox.txt" org-directory))
-  ;; (setq org-agenda-file-reading (expand-file-name "0x00_GTD/05_reading.org" org-directory))
-  ;; (setq org-agenda-files (list (concat org-directory "/0x00_GTD")))
 
   (setq org-agenda-files (apply 'append
                                 (mapcar
